0193d64af060cfe2b2d5f91b7dabd5d79c0fcdbc
Christopher Melen <chris_melen@yahoo.com>
2025-06-23 16:19:31 +0100

    Updated kmeans.py so that it sets the filename column as the dataframe index column.

--- kmeans.py
+++ kmeans.py
@@ -20,6 +20,6 @@ def get_kmeans_clusters(input_dir, sr=16000, k=5):
                 feature_list = ["mel_spectrogram", "spectral_centroid", "spectral_flux"]
                 feat.append(extract_features(filepath, 0, audio, sr, feature_list, 2048, 2048))
     df = features_to_dataframe(feat)
+    df.set_index( ['filename'], inplace=True)
+    df.drop(['timestamp'], axis=1, inplace=True)
     return kmeans_cluster(df, k)
-
-                
