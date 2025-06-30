d3839b7fd8a8d09dc2fa09ca7f405eee79f203d1
Christopher Melen <chris_melen@yahoo.com>
2025-06-30 02:59:23 +0100

    Fixed some typos in fingerprint_extractor.py.

--- fingerprint_extractor.py
+++ fingerprint_extractor.py
@@ -3,19 +3,21 @@
 class FingerprintExtractor:
 
     def __init__(self, peaks):
-        self.peaks = peaks
+        self.peaks = [
+            {'time': t, 'bin': f, 'magnitude': m, 'usages': 0} for (t, f, m) in peaks
+        ]
 
-        self.max_event_point_usages
+        self.max_event_point_usages = 10
         self.audio_block_index = 0
 
-        self.min_time_distance
-        self.max_time_distance
-        self.min_freq_distance
-        self.max_freq_distance
+        self.min_time_distance = 2
+        self.max_time_distance = 33
+        self.min_freq_distance = 1
+        self.max_freq_distance = 128
 
         self.fingerprints = []
         self.fingerprint_index = 0
-        self.max_fingerprints
+        self.max_fingerprints = 300
 
 
     def extract(self):
@@ -25,7 +27,7 @@ class FingerprintExtractor:
         for (i, peak) in enumerate(peaks):
 
             t1, f1, m1 = peaks[i]
-            u1 = peaks[i].usages
+            u1 = peaks[i]['usages']
 
             # Do not evaluate empty points.
             if (f1==0 and t1==0) : break
@@ -40,7 +42,7 @@ class FingerprintExtractor:
             for j in range(i+1, len(peaks)):
                 
                 t2, f2, m2 = peaks[j]
-                u2 = peaks[j].usages
+                u2 = peaks[j]['usages']
 
                 f_diff = abs(f1 - f2)
                 t_diff = t2 - t1
@@ -54,29 +56,26 @@ class FingerprintExtractor:
                 # Do not evaluate points to far in the future.
                 if (t_diff > self.max_time_distance) : break
 
-                if (t_iff >= self.min_time_distance and t_iff <= self.max_time_distance and
+                if (t_diff >= self.min_time_distance and t_diff <= self.max_time_distance and
                     f_diff >= self.min_freq_distance and f_diff <= self.max_freq_distance):
 
                     assert(t2>t1)
 
                     if (self.fingerprint_index == self.max_fingerprints):
-                        print(f"Warning: Fingerprint maximum index {self.fingerprint_index} reached, fingerprints are ignored, consider increasing max_fingerprints if you see this often.")
+                        print(f'Warning: Fingerprint maximum index {self.fingerprint_index} reached, fingerprints are ignored, consider increasing max_fingerprints if you see this often.')
                     else:
-                        self.fingerprints[self.fingerprint_index].time1 = t1
-                        self.fingerprints[self.fingerprint_index].time2 = t2
-                        self.fingerprints[self.fingerprint_index].time3 = t2
-
-                        self.fingerprints[self.fingerprint_index].bin1 = f1
-                        self.fingerprints[self.fingerprint_index].bin2 = f2
-                        self.fingerprints[self.fingerprint_index].bin3 = f2;
-                        
-                        self.fingerprints[self.fingerprint_index].magnitude1 = m1
-                        self.fingerprints[self.fingerprint_index].magnitude2 = m2
-                        self.fingerprints[self.fingerprint_index].magnitude3 = m2
+                        self.fingerprints[self.fingerprint_index] = {
+                            'time1': t1,
+                            'time2': t2,
+                            'bin1': f1,
+                            'bin2': f2,
+                            'magnitude1': m1,
+                            'magnitude2': m2,
+                        }
 
                         # Count event point usages.
-                        self.peaks[i].usages += 1
-                        self.peaks[j].usages += 1
+                        self.peaks[i]['usages'] += 1
+                        self.peaks[j]['usages'] += 1
 
                         self.fingerprint_index += 1
 
