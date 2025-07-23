a98ee7033fe862b4fb7928b0f075766d7f70536b
Christopher Melen <chris_melen@yahoo.com>
2025-07-23 23:02:10 +0100

    Added functions for saving audio reconstructions of source separated layers, and plotting them.

--- source_separation.py
+++ source_separation.py
@@ -1,6 +1,9 @@
 import math
+import os
 import numpy as np
+import matplotlib
 from matplotlib.pylab import plt
+import soundfile as sf
 
 
 def divergence(V, W, H, beta=2):
@@ -33,34 +36,42 @@ def source_separation(V, S, beta=2, threshold=0.05, max_iter=6000):
 
     return W, H, cost_function
 
-'''
-plt.plot(range(0,2001), cost_function, label="Cost Function")
 
-f, axs = plt.subplots(nrows=1, ncols=1, figsize=(20,5))
-D = librosa.amplitude_to_db(W, ref=np.max)
-librosa.display.specshow(D, y_axis="hz", sr=16000, hop_length=512, x_axis="time", cmap=matplotlib.cm.jet, ax=axs)
-plt.show()
-
-def filtered_spectrogram(W, H, S=2, sr=16000, hop_length=512):
-    f, axs = plt.subplots(nrows=1, ncols=S, figsize=(20,5))
-    filtered_spectrograms = []
-    for i in range(S):
-        axs[i].set_title(f"Frequency Mask of Audio Source s = {i+1}") 
-        # Filter each source component
-        filtered_spectrogram = W[:,[i]]@H[[i],:]
-        # Compute the filtered spectrogram
-        D = librosa.amplitude_to_db(filtered_spectrogram, ref=np.max)
-        # Show the filtered spectrogram
-        librosa.display.specshow(D, y_axis="hz", sr=sr, hop_length=hop_length, x_axis="time", cmap=matplotlib.cm.jet, ax=axs[i])
-        filtered_spectrograms.append(filtered_spectrogram)
-    #return filtered_spectrograms
+def plot_cost_function(cost_function):
+    plt.plot(range(len(cost_function)), cost_function, label="Cost Function")
     plt.show()
 
-def reconstruct_audio(filtered_spectrograms, n_fft=1024, hop_length=512):
-    reconstructed_audio = []
+def get_filtered_stfts(W, H, S=2, sr=16000, hop_length=512):
+    filtered_stfts = []
     for i in range(S):
-        reconstruct = filtered_spectrograms[i] * np.exp(1j*sound_stft_angle)
-        new_sound   = librosa.istft(reconstruct, n_fft=n_fft, hop_length=hop_length)
-        reconstructed_audio.append(new_sound)
-    return reconstructed_audio
+        # Filter each source component
+        filtered_stft = W[:,[i]]@H[[i],:]
+        filtered_stfts.append(filtered_stft)
+    return filtered_stfts
+
+def plot_filtered_stfts(filtered_stfts, sr=16000, hop_length=512):
+    S = len(filtered_stfts)
+    f, axs = plt.subplots(nrows=1, ncols=S, figsize=(20,5))
+    for (i, stft) in enumerate(filtered_stfts):
+        axs[i].set_title(f"Frequency Mask of Audio Source s = {i+1}")
+        # Compute the filtered spectrogram
+        D = librosa.amplitude_to_db(stft, ref=np.max)
+        # Show the filtered spectrogram
+        librosa.display.specshow(D, y_axis="hz", sr=sr, hop_length=hop_length, x_axis="time", cmap=matplotlib.cm.jet, ax=axs[i])
+    plt.show()
+
+def reconstruct_audio(filtered_stfts, phase_stft, output_dir, sr=16000, fft_size=1024, hop_length=512):
+    for (i, stft) in enumerate(filtered_stfts):
+        reconstruct = stft * np.exp(1j * phase_stft)
+        layer = librosa.istft(reconstruct, n_fft=fft_size, hop_length=hop_length)
+        sf.write(os.path.join(output_dir, f"layer_{i}.wav"), layer, sr)
+
+'''
+audio, _ = librosa.load("test5/16000kHz/chunks/005_Disintegration_(op.10)_chunk_3.wav", mono=True, sr=16000)
+V = librosa.stft(audio, n_fft=1024, hop_length=512)
+phase_stft = np.angle(V)
+V = np.abs(V)
+V = V + 1e-10
+
+W, H, cost_function = source_separation(V, 2, beta=2, threshold=0.05, max_iter=2000)
 '''
